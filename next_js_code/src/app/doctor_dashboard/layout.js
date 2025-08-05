8667e87c3d34c298aa4486c725875ff82aae74ac
ABHISHEKGORE2305 <abhishek.gore2305@gmail.com>
2025-08-06 02:20:48 +0530

    Created Doctor Dashboard

---
+++ next_js_code/src/app/doctor_dashboard/layout.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Button from '../../components/ui/button';
+
+export default function UserDashboardLayout({ children }) {
+    const componentName = children.type?.name || "UnknownComponent";
+    console.log(`Rendering component: ${componentName}`);
+    return (
+        <div className="h-screen w-screen overflow-hidden flex rounded-2xl">
+            <div className="h-full w-1/6 bg-blue-500 p-4 space-y-4 font-extrabold">
+            <div>
+                <h1 className="text-white text-3xl text-center">CLINIK</h1>
+                <hr className="border-slate-100"></hr>
+                <ul className="text-white text-lg">
+                    <li className="hover:bg-blue-600 p-2 rounded font-thin"><Link href='/user_dashboard'>Dashboard</Link></li>
+                    
+                    <li className="hover:bg-blue-600 p-2 rounded"><Link href='/user_dashboard/appointments'>Appointments</Link></li>
+                    <li className="hover:bg-blue-600 p-2 rounded"><Link href='/profile'>Profile</Link></li>
+                    <li className="hover:bg-blue-600 p-2 rounded"><Link href='/settings'>Settings</Link></li>
+                </ul>
+                <hr className="border-slate-100 mt-4"></hr>
+            </div>
+                
+                <div className="hover:bg-blue-600 p-2 rounded translate-y-[400%]"><Button  /></div>
+            
+            </div>
+            <div className="h-full w-5/6">
+                {children}
+            </div>
+        </div>
+    );
+}
--- next_js_code/src/components/ui/calender.js
+++ next_js_code/src/components/ui/calender.js
@@ -6,10 +6,17 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
 
+const today = new Date();
+    const year = today.getFullYear();
+        const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+        const day = today.getDate().toString().padStart(2, '0');
+
+        const customFormat = `${year}-${month}-${day}`;
+
 export default function CalendarClient() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar defaultValue={dayjs('2025-08-02')} className='h-full'/>
+      <DateCalendar defaultValue={dayjs(customFormat)} className='h-full'/>
     </LocalizationProvider>
   );
 }
